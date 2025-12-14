e8d9f66c4bd6f7497131ff73c7fa3983f51a3b42
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-15 00:17:11 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,5 @@
-const CACHE = "mf-analytics-v1";
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.addAll([
-        "./",
-        "./index.html"
-      ])
-    )
-  );
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open('mf').then(c=>c.add('/')));
 });
 
 self.addEventListener("fetch", e => {
