d734e5243a0ecaa7a18dbfb5ac4365881f1c7646
system4trading <104557852+system4trading@users.noreply.github.com>
2025-12-15 18:33:48 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,3 @@
-export function xirr(cashflows){
-  let rate=0.1;
-  for(let i=0;i<100;i++){
-    let f=0,df=0;
-    cashflows.forEach(c=>{
-      f+=c.amount/((1+rate)**(c.t/365));
-      df+=-c.amount*(c.t/365)/((1+rate)**((c.t/365)+1));
-    });
-    rate-=f/df;
-  }
-  return rate;
+export function savePortfolio(p) {
+  localStorage.setItem("portfolio", JSON.stringify(p));
 }
-
